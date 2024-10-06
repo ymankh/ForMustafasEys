7c32578c20a046138f6e48cf90793190361d1c4b
Yaman Khashashneh <ymankh1997@gmail.com>
2024-10-06 15:30:18 +0300

    add the dummy controller

---
+++ ForMustafasEys.Server/Controllers/DummyApiController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ForMustafasEys.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DummyApiController : ControllerBase
+    {
+
+    }
+}
