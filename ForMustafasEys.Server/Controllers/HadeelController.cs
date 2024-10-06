3801939afd5efacccda6365552a348b58cd1453e
Hadeel-Alzoubi <hadeelzoubi12@gmail.com>
2024-10-06 16:10:45 +0300

    HADEEL

---
+++ ForMustafasEys.Server/Controllers/HadeelController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ForMustafasEys.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HadeelController : ControllerBase
+    {
+        // HADEEEEEEEEEEEEEL 
+        //ALZOUBI
+        string str = "hadeel";
+    }
+}
